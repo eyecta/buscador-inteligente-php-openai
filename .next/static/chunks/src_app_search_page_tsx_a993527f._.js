998f937c8d4dbd9805dd6f0e0fa59481e75236d4
Eyecta Studio <152719844+eyecta@users.noreply.github.com>
2025-06-01 02:36:50 +0200

    Add .next/static/chunks/src_app_search_page_tsx_a993527f._.js 1748738210516

---
+++ .next/static/chunks/src_app_search_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_881658ee._.js",
+  "static/chunks/src_bffe3511._.js"
+],
+    source: "dynamic"
+});
